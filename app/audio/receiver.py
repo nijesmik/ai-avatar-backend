81ed7b8a755483269d1a860529ec9316d63368b6
nijesmik <bkybb@duck.com>
2025-04-10 03:19:44 +0900

    :sparkles: WebRTC VAD를 활용해 음성 감지 기능을 구현

--- app/audio/receiver.py
+++ app/audio/receiver.py
@@ -1,13 +1,40 @@
 from aiortc import MediaStreamTrack
+import logging
+import webrtcvad
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
 
 class AudioReceiverTrack(MediaStreamTrack):
     kind = "audio"
+    vad = webrtcvad.Vad(3)
 
     def __init__(self, track, sid):
         super().__init__()
         self.track = track
         self.sid = sid
+        self.in_speech = False
+        self.speech_count = 0
 
     async def recv(self):
         frame = await self.track.recv()
+        pcm = frame.to_ndarray().tobytes()
+        chunk = pcm[:320]
+        is_speech = self.vad.is_speech(chunk, 16000)
+
+        if is_speech:
+            if not self.in_speech:
+                logger.debug("🟢 발화 시작")
+                self.in_speech = True
+
+            self.speech_count = 0
+
+        elif self.in_speech:
+            self.speech_count += 1
+            if self.speech_count > 50:  # 50 * 20ms = 1s
+                logger.debug("🔴 발화 종료")
+                self.in_speech = False
+                self.speech_count = 0
+
         return frame
