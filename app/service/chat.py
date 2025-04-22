eebe864159bbec48e77b58d62ba39ca5b548aae6
nijesmik <bkybb@duck.com>
2025-04-23 02:01:24 +0900

    :sparkles: Google AI 기반 채팅 서비스 클래스 구현 - 필요한 라이브러리 및 의존성 업데이트

---
+++ app/service/chat.py
@@ -0,0 +1,68 @@
+import logging
+import re
+from os import getenv
+
+from google import genai
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+system_instruction = f"""
+You are a friendly AI avatar having a voice conversation with the user.
+
+Please follow these rules when generating your response:
+
+1. Do not use emojis (e.g., 😊, 🤖) or decorative symbols (e.g., ★, ♡).
+2. Keep your sentences short and natural — like spoken language.
+3. Avoid slang, abbreviations, or foreign words that might confuse speech synthesis.
+4. Use proper punctuation like commas and periods for natural rhythm.
+5. The response will be spoken using text-to-speech (TTS), so make sure all words are clearly pronounceable.
+
+Please respond naturally with 2 to 4 well-formed sentences — not too short, not too long.
+"""
+
+
+class ChatService:
+    client = genai.Client(api_key=getenv("GEMINI_API_KEY"))
+    regex = re.compile(r"(.*?[\.!?])(\s+|$)")
+
+    def __init__(self, model: str = "gemini-2.0-flash-lite"):
+        self.chat = self.client.aio.chats.create(model=model)
+
+    async def send_message(self, message: str):
+        response = await self.chat.send_message_stream(
+            message,
+        )
+
+        async for chunk in response:
+            logger.debug(f"💬 text: {chunk.text}")
+            yield chunk.text
+
+    async def send_utterance(self, utterance: str):
+        response = await self.chat.send_message_stream(
+            utterance,
+            config={
+                "system_instruction": system_instruction,
+            },
+        )
+
+        buffer = ""
+
+        async for chunk in response:
+            logger.debug(f"💬 text: {chunk.text}")
+            buffer += chunk.text
+
+            match = self.regex.match(buffer)
+            if not match:
+                continue
+
+            yield match.group(1).strip()
+            buffer = buffer[match.end() :]
+
+        while buffer:
+            match = self.regex.match(buffer)
+            if not match:
+                break
+
+            yield match.group(1)
+            buffer = buffer[match.end() :]
